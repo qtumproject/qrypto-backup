6d0370bac0958a4e8d890153ac789c6c1fab8f4e
Jerry Chen <chen.ruijie@gmail.com>
2018-06-05 18:52:08 +0800

    remove unused code

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,23 +1,6 @@
 import * as React from 'react'
 import { networks, Wallet, Insight} from 'qtumjs-wallet'
 
-// import SendEther from './pages/sendEther'
-
-// import HDWallet from '../HDWallet'
-
-// import { utils } from 'ethers'
-// import { getBalance } from '../getBalance'
-
-let storageMnemonic: string = ''
-// let storageWallet: HDWallet
-chrome.storage.local.get('mnemonic', ({ mnemonic }) => {
-  if (mnemonic == null) {
-    return
-  }
-
-  storageMnemonic = mnemonic
-  // storageWallet = new HDWallet(mnemonic)
-})
 
 function recoverWallet(mnemonic: string): Wallet {
   const network = networks.testnet
@@ -25,38 +8,20 @@ function recoverWallet(mnemonic: string): Wallet {
 }
 
 class App extends React.Component<IProps, IState> {
-  private hasUnmounted = false
+
   public constructor(props: IProps) {
     super(props)
 
     this.state = {
       mnemonic: 'hold struggle ready lonely august napkin enforce retire pipe where avoid drip',
-      amount: 0
-      // wallet: undefined,
-      // balances: {},
+      amount: 0,
       tip: ''
     }
   }
 
   public componentDidMount() {
-    // if (this.state.mnemonic !== '') {
-    //   return
-    // }
-
-    // chrome.storage.local.get('mnemonic', ({ mnemonic }) => {
-    //   if (mnemonic == null || this.hasUnmounted) {
-    //     return
-    //   }
-
-    //   const wallet = new HDWallet(mnemonic)
-
-    //   this.setState({ mnemonic, wallet })
-    // })
+    
   }
-
-  // public componentWillUnmount() {
-  //   this.hasUnmounted = true
-  // }
 
   private async getWalletInfo(wallet: Wallet) {
     const info = await wallet.getInfo()
@@ -65,9 +30,7 @@ class App extends React.Component<IProps, IState> {
 
   public renderWallet() {
     const {info, tip} = this.state;
-    // if(wallet) {
-    //   wallet.
-    // }
+
     return (
       <div>
         {info && this.renderInfo()}
@@ -120,29 +83,29 @@ class App extends React.Component<IProps, IState> {
     this.getWalletInfo(wallet)
   }
 
-  private async handleSendTo = () => {
+  private handleSendTo =  async () => {
     this.setState({ tip: 'sending...'});
 
     const { receiver, amount } = this.state
     
     const wallet = this.state.wallet!
 
-    const tx = wallet.send(receiver, amount * 1e8, {
-      feeRate: 400
-    })
-
-    this.setState({ tip: 'done' })
-    
-    tx.catch(err => {
+    try {
+      await wallet.send(receiver, amount * 1e8, {
+        feeRate: 400
+      })
+  
+      this.setState({ tip: 'done' })
+    } catch (err) {
       console.log(err)
       this.setState({ tip: err.message })
-    })
+    }
+
   }
 
   public render() {
-    // const mnemonic = 'foobar'
 
-    const { mnemonic, wallet, info } = this.state
+    const { mnemonic, wallet } = this.state
 
     return (
       <div>
@@ -154,91 +117,6 @@ class App extends React.Component<IProps, IState> {
         {wallet && this.renderWallet()}
       </div>
     )
-    // const { page, usingAddress, wallet, balances, mnemonic } = this.state
-
-    // if (page != null) {
-    //   switch (page) {
-    //     case 'sendEther':
-    //       if (usingAddress == null || wallet == null || balances[usingAddress] == null) {
-    //         throw new Error('cannot render sendEther page')
-    //       }
-    //       return (
-    //         <SendEther
-    //           address={usingAddress}
-    //           port={this.props.port}
-    //           hdWallet={wallet}
-    //           balance={balances[usingAddress]}
-    //         />
-    //       )
-
-    //     default:
-    //   }
-    // }
-
-    // return (
-    //   <div className={'container'}>
-    //     {this.renderAccounts()}
-    //     <input type="text" onChange={this.handleInputChange} value={mnemonic} />
-    //     <button onClick={this.handleRecover}>
-    //       recover
-    //     </button>
-    //     <button onClick={this.handleReset}>
-    //       reset
-    //     </button>
-    //   </div>
-    // )
-  }
-
-  private renderAccounts() {
-    const { wallet } = this.state
-    if (wallet == null) {
-      return null
-    }
-
-    return (
-      wallet.addresses.map((address) => (
-        <div key={address}>
-          {address}
-          {' '}
-          {this.renderBalance(address)}
-        </div>
-      ))
-    )
-  }
-
-  private renderBalance(address: string) {
-    const balance = this.state.balances[address]
-    if (this.state.balances[address] == null) {
-      this.fetchBalance(address)
-      return
-    }
-
-    const balanceStr = utils.formatEther(utils.bigNumberify(balance))
-    const [intergerPart, decimalPart] = balanceStr.split('.')
-
-    return (
-      <>
-        <span>
-          Balance: {intergerPart}{decimalPart == null ? null : `.${decimalPart.slice(0, 3)}`}
-        </span>
-        <button onClick={() => this.handleSend(address)}>
-          Transfer
-        </button>
-      </>
-    )
-  }
-
-  private async fetchBalance(address: string) {
-    const balance = await getBalance(this.props.port, address)
-    if (this.hasUnmounted) {
-      return
-    }
-
-    this.setState(({ balances }) => ({
-      balances: Object.assign(balances, {
-        [address]: balance,
-      }),
-    }))
   }
 
   private handleInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
@@ -251,25 +129,11 @@ class App extends React.Component<IProps, IState> {
     const { mnemonic } = this.state
 
     try {
-      // const wallet = new HDWallet(mnemonic)
       const wallet = recoverWallet(mnemonic)
-      // chrome.storage.local.set({ mnemonic })
       this.getWalletInfo(wallet)
     } catch (err) {
       console.log('cannot set mnemonic', err)
     }
-  }
-
-  private handleReset: React.MouseEventHandler<HTMLButtonElement> = () => {
-    chrome.storage.local.remove('mnemonic')
-    this.setState({ mnemonic: '', wallet: undefined })
-  }
-
-  private handleSend = (address: string) => {
-    this.setState({
-      page: 'sendEther',
-      usingAddress: address,
-    })
   }
 }
 
@@ -279,14 +143,11 @@ interface IProps {
 
 interface IState {
   mnemonic: string
-  // balances: { [address: string]: string }
   wallet?: Wallet
   info?: Insight.IGetInfo
   receiver: string
   amount: number
   tip: string
-  // page?: string
-  // usingAddress?: string
 }
 
 export default App
